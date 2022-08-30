c1c7df37145075957985b35bcbcfa058aece9ac3
Tiago Braga <tiagohbraga@gmail.com>
2022-08-30 14:34:28 -0300

    Modificação no exercicio 03

---
+++ exercicio03/script.js
@@ -0,0 +1,5 @@
+const operacao1 = 5 > 20 && 5 < 2
+const operacao2 = 5 == 5 || 5  == "5"
+const operacao3 = 20 >! 50
+const operacao4 = 20 >! 50 || 50 >! 60
+console.log(operacao1, operacao2, operacao3, operacao4)
